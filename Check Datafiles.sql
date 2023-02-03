904e6e0c039b93fca67a5c1faa48bedcc479670e
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-02-03 01:07:59 -0300

    Update Check Datafiles.sql

--- Check Datafiles.sql
+++ Check Datafiles.sql
@@ -1,12 +1,3 @@
-col tablespace for a10
-SELECT d.con_id,A.tablespace_name tablespace, D.mb_total,
-SUM (A.used_blocks * D.block_size) / 1024 / 1024 mb_used,
-D.mb_total - SUM (A.used_blocks * D.block_size) / 1024 / 1024 mb_free
-FROM v$sort_segment A,
-(
-SELECT b.con_id, b.tablespace_name, C.block_size, SUM (C.bytes) / 1024 / 1024 mb_total
-FROM cdb_temp_files B, v$tempfile C
-WHERE B.con_id= C.con_id and b.file_name = c.name
-GROUP BY b.con_id,b.tablespace_name, C.block_size) D
-WHERE A.tablespace_name = D.tablespace_name
-GROUP by D.con_id,A.tablespace_name, D.mb_total;
+ col PDB_NAME format a8
+col FILE_NAME format a37
+ SELECT p.PDB_ID, p.PDB_NAME, d.FILE_ID, d.TABLESPACE_NAME, d.FILE_NAME FROM DBA_PDBS p, CDB_DATA_FILES d ORDER BY p.PDB_ID;
